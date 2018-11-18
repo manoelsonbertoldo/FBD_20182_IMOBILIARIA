31d73b85ddb3e21797288313c8fe7e22b7d28e24
Manoelson bertoldo <manoelsonbertoldo007@gmail.com>
2018-11-18 16:58:20 -0300

    insert no sql util

--- src/sql/SQLUtil.java
+++ src/sql/SQLUtil.java
@@ -1,12 +1,61 @@
 package sql;
 
 public class SQLUtil {
-	public static class Produto {
+	public static class Apartamento {
 
-        public static final String NOME = "produto";
-        public static final String DESCRICAO = "descricao";
-        public static final String INSERT = "insert into " + NOME + "(" + DESCRICAO + ") values (?)";
+        public static final String NOME_TABELA = "apartamento";
+        public static final String COL_PREDIO = "predio";
+        public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_PREDIO + ") values (?)";
     }
+	public static class Casa {
+		
+        public static final String NOME_TABELA = "casa";
+        public static final String COL_AREACONSTRUIDA = "areconstruida";
+        public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_AREACONSTRUIDA + ") values (?)";
+    }
+	public static class Cliente {
+		
+		public static final String NOME_TABELA = "cliente";
+		public static final String COL_CPF = "nome";
+		public static final String COL_NOME = "cpf";
+		public static final String COL_TIPO = "tipo";
+		public static final String COL_DOCUMENTO = "documento";
+		
+		
+		public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_CPF + ","
+				+ COL_NOME + ","
+				+ COL_TIPO + ","
+				+ COL_DOCUMENTO +"" 
+				+ " ) values (?,?,?,?) ";
+	}
+	
+	public static class Contato {
+		
+		public static final String NOME_TABELA = "contato";
+		public static final String COL_TIPO = "tipo";
+		public static final String COL_DESCRICAO = "descricao";
+		
+		
+		public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_TIPO + ","
+				+ COL_DESCRICAO + ""
+				 + " ) values (?,?,?)";
+	}
+public static class Corretor {
+		
+		public static final String NOME_TABELA = "corretor";
+		public static final String COL_CPF = "nome";
+		public static final String COL_NOME = "cpf";
+		public static final String COL_TIPO = "tipo";
+		public static final String COL_DOCUMENTO = "documento";
+		
+		
+		public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_CPF + ","
+				+ COL_NOME + ","
+				+ COL_TIPO + ","
+				+ COL_DOCUMENTO +"" 
+				+ " ) values (?,?,?,?) ";
+	}
+	
 
     public static class Endereco {
 
@@ -23,34 +72,94 @@ public class SQLUtil {
                 + COL_BAIRRO + ","
                 + COL_ESTADO + "" + " ) values (?,?,?,?,?) returning id";
     }
+    public static class Estado {
 
-    public static class Contato {
-
-        public static final String NOME_TABELA = "contato";
+        public static final String NOME_TABELA = "estado";
+        public static final String COL_NOME = "NOME";
+        public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_NOME + ") values (?)";
+    }
+    public static class Imoveis {
+    	
+       	
+        public static final String NOME_TABELA = "imoveis";
         public static final String COL_TIPO = "tipo";
-        public static final String COL_DESCRICAO = "descricao";
-        public static final String COL_CLIENTE_ID = "cliente_id";
+        public static final String COL_RGIMOVEIS = "rgImoveis";
+        public static final String COL_LARGURA = "largura";
+        public static final String COL_COMPRIMENTO = "comprimento";
+        public static final String COL_STATUS = "status";
+        public static final String COL_QNTCOMODOS = "qntcomodos";
+        public static final String COL_QNTQUARTOS = "qntQuartos";
+        public static final String COL_QNTSALAS = "qntSalas";
+        public static final String COL_SUITES = "suites";
+        public static final String COL_GARAGES = "garages";
+        public static final String COL_QNTBANHEIROS = "qntBanheiros";
+        public static final String COL_AREATOTAL = "areaTotal";
+        public static final String COL_VALOR = "valor";
 
         public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_TIPO + ","
-                + COL_DESCRICAO + ","
-                + COL_CLIENTE_ID + " ) values (?,?,?)";
+                + COL_RGIMOVEIS + ","
+                + COL_LARGURA + ","
+                + COL_COMPRIMENTO + ","
+                + COL_STATUS + ","
+                + COL_QNTCOMODOS + ","
+                + COL_QNTQUARTOS + ","
+                + COL_QNTSALAS+ ","
+                + COL_SUITES + ","
+                + COL_GARAGES + ","
+               
+                + COL_QNTBANHEIROS + ","
+                + COL_AREATOTAL + ","
+                
+                + COL_VALOR + "" + " ) values (?,?,?,?,?,?,?,?,?,?,?,?,?) returning id";
     }
+    public static class municipio {
 
-    public static class Cliente {
-
-        public static final String NOME_TABELA = "cliente";
-        public static final String COL_NOME = "nome";
-        public static final String COL_CPF = "cpf";
-        public static final String COL_TIPO = "tipo";
-        public static final String COL_DOCUMENTO = "documento";
-        public static final String COL_ENDERECO_ID = "endereco_id";
-
-        public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_NOME + ","
-                + COL_CPF + ","
-                + COL_TIPO + ","
-                + COL_DOCUMENTO + ","
-                + COL_ENDERECO_ID + "" + " ) values (?,?,?,?,?) returning id";
+        public static final String NOME_TABELA = "municipio";
+        public static final String COL_NOME = "NOME";
+        public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_NOME + ") values (?)";
     }
+public static class Proprietario {
+		
+		public static final String NOME_TABELA = "proprietario";
+		public static final String COL_CPF = "nome";
+		public static final String COL_NOME = "cpf";
+		public static final String COL_TIPO = "tipo";
+		public static final String COL_DOCUMENTO = "documento";
+		
+		
+		public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_CPF + ","
+				+ COL_NOME + ","
+				+ COL_TIPO + ","
+				+ COL_DOCUMENTO +"" 
+				+ " ) values (?,?,?,?) ";
+	}
+public static class Vender {
+	
+   	
+    public static final String NOME_TABELA = "vender";
+    public static final String COL_NUMVENDAS = "numVendas";
+    public static final String COL_VALORVENDAS = "valorVendas";
+    public static final String COL_VALORTOTAL = "valorTotal";
+    public static final String COL_DESCONTO = "desconto";
+    public static final String COL_TIPO = "tipo";
+    public static final String COL_FORMAPAGAMENTO = "formaPagamento";
+    public static final String COL_DATAVENDA = "dataVenda";
+    
+
+    public static final String INSERT = "insert into " + NOME_TABELA + "(" + COL_NUMVENDAS + ","
+            + COL_VALORVENDAS + ","
+            + COL_VALORTOTAL + ","
+            + COL_DESCONTO + ","
+            + COL_TIPO + ","
+            + COL_FORMAPAGAMENTO + ","
+            + COL_DATAVENDA + ""
+            
+            
+             + " ) values (?,?,?,?,?,?,?,) returning id";
+}
+	
+    
+
 
     public static String selectAll(String nomeTabela) {
         return "select * from " + nomeTabela;
